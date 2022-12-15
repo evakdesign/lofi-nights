9cbd68aea468ea03e2208e032aa31da05a67f688
Ava Blake <avablakedesign@protonmail.com>
2022-12-15 14:52:54 -0500

    Updated, heroku connection.

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,16 @@
 const Sequelize = require('sequelize')
 
 require('dotenv').config();
-const sequelize = new Sequelize(
-    {
-        database:process.env.DB_NAME,
-        password:process.env.DB_PASSWORD,
-        username:process.env.DB_USER,
-        host: 'localhost',
-        dialect: 'mysql',
-        port: 3306,
+const sequelize = new Sequelize(process.env.DB_STRING, 
+    // {
+    //     // database:process.env.DB_NAME,
+    //     // password:process.env.DB_PASSWORD,
+    //     // username:process.env.DB_USER,
+    //     // host: 'localhost',
+    //     // dialect: 'mysql',
+    //     // port: 3306,
 
-    }
+    // }
 );
 
 module.exports = sequelize;
