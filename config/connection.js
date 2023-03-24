12c6f98276e02b947e931d7083f439acc2217e28
Ava Blake <avablakedesign@protonmail.com>
2023-03-24 19:16:48 -0400

    Changed connection string.

--- config/connection.js
+++ config/connection.js
@@ -2,15 +2,7 @@ const Sequelize = require('sequelize')
 
 require('dotenv').config();
 const sequelize = new Sequelize( 
-    {
-        database: process.env.database,
-        password:process.env.password,
-        username:'root',
-        host: process.env.host,
-        dialect: 'mysql',
-        port: process.env.port,
-
-    }
+    process.env.DB_STRING
 );
 
 module.exports = sequelize;
