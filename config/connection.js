fbf7a907eb4271e18fa5bba1da944181d4c41bcd
Ava Blake <avablakedesign@protonmail.com>
2023-03-24 19:38:06 -0400

    enable ssl for database.

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,16 @@ const Sequelize = require('sequelize')
 
 require('dotenv').config();
 const sequelize = new Sequelize( 
-    process.env.DB_STRING
+    process.env.DB_STRING,
+    {
+        dialect: "postgres",
+        protocol: "postgres",
+        dialectOptions: {
+            ssl:{
+                require:"true"
+            }
+        }
+    }
 );
 
 module.exports = sequelize;
